c4bae8ef64071cbdcf0c384cefa8894bfc9bbf46
Student <student@CA-iMac-10.local>
2012-10-27 20:44:01 -0500

    fixed location parsing

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -42,10 +42,10 @@ class CareerController < ApplicationController
 
 		@job_title2 = @job_title.to_s.gsub(/,*\s+/,'+')
 		@company_name2 = @company_name.to_s.gsub(/,*\s+/,'+')
-		@location2 = @location2.to_s.gsub(/,*\s+/,'+')
+		@location2 = @location.to_s.gsub(/,*\s+/,'+')
 
-		url = "http://api.careerbuilder.com/v1/jobsearch?DeveloperKey=WDTX43Z6Z0TD34QLFVRL&JobTitle=#{@job_title2}&CompanyName=#{@company_name2}&Location=#{@location2}&OutputJson=true" 
-		json_response = open(url).read
+		@url = "http://api.careerbuilder.com/v1/jobsearch?DeveloperKey=WDTX43Z6Z0TD34QLFVRL&JobTitle=#{@job_title2}&CompanyName=#{@company_name2}&Location=#{@location2}&OutputJson=true" 
+		json_response = open(@url).read
 		output = JSON.parse(json_response)
 		@search_results = output["ResponseJobSearch"]["Results"]["JobSearchResult"]
 
